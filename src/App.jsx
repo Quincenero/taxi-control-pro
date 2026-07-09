923354437e15ead11b8181d98682263cd77159f4
Marco <mespinozapoma@gmail.com>
2026-07-08 21:40:56 -0300

    refactor: reorganizar estructura inicial del proyecto

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
-import Header from "./components/Header/Header";
-import FormularioViaje from "./components/FormularioViaje/FormularioViaje.jsx";
-import Resumen from "./components/Resumen/Resumen.jsx";
+import Header from "./components/layout/Header/Header.jsx";
+import FormularioViaje from "./components/viajes/FormularioViaje/FormularioViaje.jsx";
+import Resumen from "./components/resumen/Resumen/Resumen.jsx";
 
 function App() {
   const [ingresos, setIngresos] = useState(0);
